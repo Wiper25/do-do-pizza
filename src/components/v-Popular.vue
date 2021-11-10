33ac334a466063cb15001ca70e24dbcffe1327b4
wiper <wiper996100@gmail.com>
2021-11-10 18:00:36 +0600

    Добавил слайдер популярных заказов, сделал header

--- src/components/v-Popular.vue
+++ src/components/v-Popular.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="v-Popular">
-    <div class="v-Popular__slider"></div>
+    <div class="v-Popular__slider">
+      <div class="v-Popular__cards" :key="i" v-for="(card, i) in cards">
+        <div class="v-Popular__card">
+          <img width="80" height="80" :src="images" alt="" />
+          <div class="v-Popular__card-text">
+            <div class="v-Popular__card-title">{{ card.name }}</div>
+            <div class="v-Popular__card-price">{{ card.price }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import popularContent from "../json-content/popular-content.json";
+import img from "../assets/730db614-8678-4465-859c-bd8c9d8ef427.jpg";
 export default {
   data() {
-    return {};
+    return {
+      cards: popularContent,
+      images: img,
+    };
   },
   methods: {},
 };
